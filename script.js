ebd8cc0da2ef03b542d3933e93a8dec5f4e5019d
Siddharth-ABESEC <siddharthsingh21106@gmail.com>
2026-02-25 09:54:19 +0530

    index

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+        let a=10;
+        console.log(a);
+        console.log(type(a));
+        a=false;
+        console.log(a);
+        console.log(type(a));
+        a="Hello World"
+        console.log(a);
+        console.log(type(a));
+        a=null;
+        console.log(a);
+        console.log(type(a));
